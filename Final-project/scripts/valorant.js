0bf0a6db91071f7b84ef98a9aaf79072e493f56a
will-mon <willymoncada02@gmail.com>
2025-02-19 18:49:33 -0500

    final-project

---
+++ Final-project/scripts/valorant.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+const copyrightYearElement = document.getElementById('currentyear');
+const lastModifiedElement = document.getElementById('lastModified');
+copyrightYearElement.textContent = currentYear;
+lastModifiedElement.textContent = `Last update: ${lastModified}`;
