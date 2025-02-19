11f58a1a36027e5026aaa6bdbdb207d66346e80e
will-mon <willymoncada02@gmail.com>
2025-02-19 02:03:14 -0500

    final-project phase 1

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Footer information
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+const copyrightYearElement = document.getElementById('currentyear');
+const lastModifiedElement = document.getElementById('lastModified');
+copyrightYearElement.textContent = currentYear;
+lastModifiedElement.textContent = `Last update: ${lastModified}`;
